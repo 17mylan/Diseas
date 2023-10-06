779d89e194c1e75ad540edb0bb59d6f5a4319a48
Gus2169 <auguste.guillot@e-artsup.net>
2023-10-06 09:20:16 +0200

    ennemie2

--- Diseas/Assets/Gus/Script/Ennemi2.cs
+++ Diseas/Assets/Gus/Script/Ennemi2.cs
@@ -3,11 +3,14 @@ using UnityEngine.AI;
 
 public class Ennemi2 : MonoBehaviour
 {
-    [Range(1f, 50f)]
-    public float detectionRange = 10f;
+    public float detectionRange = 10f; // Portée de détection
+    public float attackRange = 5f; // Portée d'attaque
+    public float fireRate = 2f; // Taux de tir en secondes
+    private float nextFireTime = 0f;
     public NavMeshAgent _AI;
     public Transform _playerReference;
-    public BulletInstantiate bulletInstantiate; // Référence au script de gestion des balles
+    public GameObject projectilePrefab; // Remplacez cela par le prefab de votre sphère 3D
+    public Transform projectileSpawnPoint;
 
     void Update()
     {
@@ -15,11 +18,18 @@ public class Ennemi2 : MonoBehaviour
 
         if (distanceToPlayer <= detectionRange)
         {
-            // Le joueur est dans la plage de détection, déplacer l'ennemi vers le joueur
-            _AI.destination = _playerReference.position;
-
-            // Tirer sur le joueur
-            bulletInstantiate.CreateBullet("WithEnemy");
+            // Le joueur est dans la plage de détection
+            if (distanceToPlayer <= attackRange && Time.time >= nextFireTime)
+            {
+                // Le joueur est également dans la plage d'attaque, tirer une sphère 3D
+                ShootProjectile();
+                nextFireTime = Time.time + 1f / fireRate; // Met à jour le prochain temps de tir
+            }
+            else
+            {
+                // Le joueur est dans la plage de détection mais pas dans la plage d'attaque, déplacer l'ennemi vers le joueur
+                _AI.destination = _playerReference.position;
+            }
         }
         else
         {
@@ -27,5 +37,20 @@ public class Ennemi2 : MonoBehaviour
             // Par exemple, arrêter de se déplacer
             _AI.destination = transform.position;
         }
+    }
+
+    void ShootProjectile()
+    {
+        // Vous devrez remplacer le "projectilePrefab" par le prefab réel que vous utilisez pour votre sphère 3D
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+
+        // Calculer la direction vers le joueur
+        Vector3 directionToPlayer = (_playerReference.position - projectileSpawnPoint.position).normalized;
+
+        // Ajoutez ici le code pour déplacer le projectile (par exemple, rigidbody.velocity pour un Rigidbody)
+        projectile.GetComponent<Rigidbody>().velocity = directionToPlayer * 20f; // Remplacez 10f par la vitesse souhaitée du projectile
+
+        // Assurez-vous de détruire le projectile après un certain temps ou lorsqu'il entre en collision avec quelque chose
+        Destroy(projectile, 5f); // Remplacez 5f par la durée de vie souhaitée du projectile
     }
 }
