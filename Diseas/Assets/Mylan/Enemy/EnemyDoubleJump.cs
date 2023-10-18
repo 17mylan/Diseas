8bc04cc66b402f11def579885c87ad2ca13ecf8c
Gus2169 <auguste.guillot@e-artsup.net>
2023-10-18 15:41:35 +0200

    double jump

--- Diseas/Assets/Mylan/Enemy/EnemyDoubleJump.cs
+++ Diseas/Assets/Mylan/Enemy/EnemyDoubleJump.cs
@@ -4,6 +4,8 @@ using UnityEngine.AI;
 public class EnemyDoubleJump : MonoBehaviour
 {
     public float detectionRange = 10f;
+    public float jumpHeight = 2f; // Hauteur des sauts
+    public float jumpSpeed = 5f; // Vitesse des sauts
     public NavMeshAgent _AI;
     public GameObject _playerReference;
     public Transform _exampleCharacter;
@@ -17,17 +19,25 @@ public class EnemyDoubleJump : MonoBehaviour
 
     void Update()
     {
-        if(canAiMove)
+        if (canAiMove)
         {
             if (!_AI.enabled)
             {
                 _AI.enabled = true;
             }
+
             float distanceToPlayer = Vector3.Distance(transform.position, _exampleCharacter.position);
 
             if (distanceToPlayer <= detectionRange)
             {
-                _AI.destination = _exampleCharacter.position;
+                // Déplacement du cube vers le joueur avec des petits sauts
+                Vector3 targetPosition = new Vector3(_exampleCharacter.position.x, _exampleCharacter.position.y, _exampleCharacter.position.z);
+                float step = jumpSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+
+                // Faites en sorte que le cube saute légèrement pendant le déplacement
+                float newY = Mathf.PingPong(Time.time * jumpSpeed, jumpHeight);
+                transform.position = new Vector3(transform.position.x, newY, transform.position.z);
             }
             else
             {
