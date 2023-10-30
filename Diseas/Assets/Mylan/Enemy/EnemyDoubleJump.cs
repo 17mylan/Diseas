ea23608995c8c7c8fbc94174df87c6b55f0092ad
Gus2169 <auguste.guillot@e-artsup.net>
2023-10-30 19:55:35 +0100

    jumpEnnemiFix

--- Diseas/Assets/Mylan/Enemy/EnemyDoubleJump.cs
+++ Diseas/Assets/Mylan/Enemy/EnemyDoubleJump.cs
@@ -4,17 +4,21 @@ using UnityEngine.AI;
 public class EnemyDoubleJump : MonoBehaviour
 {
     public float detectionRange = 10f;
-    public float jumpHeight = 2.3f; // Hauteur des sauts
-    public float jumpSpeed = 6.5f; // Vitesse des sauts
+    public float jumpHeight = 2.3f;
+    public float jumpSpeed = 6.5f;
     public NavMeshAgent _AI;
     public GameObject _playerReference;
     public Transform _exampleCharacter;
     public bool canAiMove = true;
 
+    private float originalY;
+
     public void Start()
     {
         _playerReference = GameObject.Find("ExampleCharacter");
         _exampleCharacter = _playerReference.transform;
+
+        originalY = transform.position.y;
     }
 
     void Update()
@@ -30,14 +34,24 @@ public class EnemyDoubleJump : MonoBehaviour
 
             if (distanceToPlayer <= detectionRange)
             {
-                // Déplacement du cube vers le joueur avec des petits sauts
-                Vector3 targetPosition = new Vector3(_exampleCharacter.position.x, _exampleCharacter.position.y, _exampleCharacter.position.z);
-                float step = jumpSpeed * Time.deltaTime;
-                transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+                // Rotation de l'ennemi pour faire face au joueur
+                Vector3 lookAtPlayer = new Vector3(_exampleCharacter.position.x, transform.position.y, _exampleCharacter.position.z);
+                transform.LookAt(lookAtPlayer);
 
-                // Faites en sorte que le cube saute légèrement pendant le déplacement
-                float newY = Mathf.PingPong(Time.time * jumpSpeed, jumpHeight);
-                transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+                // Déplacement du cube vers le joueur avec des petits sauts
+                float step = jumpSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, _exampleCharacter.position, step);
+
+                // Faites en sorte que le cube saute avec une courbe sinusoidale
+                float newY = originalY + Mathf.Sin(Time.time * jumpSpeed) * jumpHeight;
+
+                // Correction de la position verticale pour éviter l'enfoncement dans le sol
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity))
+                {
+                    float groundHeight = hit.point.y + GetComponent<Collider>().bounds.extents.y;
+                    transform.position = new Vector3(transform.position.x, Mathf.Max(newY, groundHeight), transform.position.z);
+                }
             }
             else
             {
