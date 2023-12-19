49241efc474295258aaa0e42f2d8f6e571671853
Gus2169 <auguste.guillot@e-artsup.net>
2023-12-19 15:21:12 +0100

    life collectible version finale

--- Diseas/Assets/Mylan/Collectible/LifeCollectible/LifeCollectible.cs
+++ Diseas/Assets/Mylan/Collectible/LifeCollectible/LifeCollectible.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class LifeCollectible : MonoBehaviour
 {
-    public float forceSaut = 5f;
+    public float forceSaut = 7f;
     public float sideImpulseRange = 2f; // Nouvelle variable pour la portée de l'impulsion sur le côté
     public GameObject _playerReference;
     public Transform _exampleCharacter;
     public float detectionRange = 7f;
     public float minSpeed = 7f;
     public float maxSpeed = 15f;
-    public float delayBeforeCollecting = 2f;
+    public float delayBeforeCollecting = 1f;
 
     private PlayerHealth playerHealth;
     private float currentSpeed;
